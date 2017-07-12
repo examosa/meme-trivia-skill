3187a03502b8a7e27f0bee9264c173d0ae94fca8
Juleo Amosah <teomik129@gmail.com>
2017-07-12 17:58:32 -0400

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Meme Trivia
+
+This project is an Alexa Skill which uses [AWS Lambda](https://aws.amazon.com/lambda/), the [Alexa Skills Kit (ASK)](https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit), and the [ASK SDK](https://developer.amazon.com/public/community/post/Tx213D2XQIYH864/Announcing-the-Alexa-Skills-Kit-for-Node-js).
+
+It was built using the [Alexa Trivia Template](https://github.com/alexa/skill-sample-nodejs-trivia).
+
+Feel free to create a pull request to add questions to the database.
---
+++ src/questions.js
@@ -0,0 +1,259 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        {
+            "Which of the following is not a famous internet cat?": [
+                "Fancy Cat",
+                "Ceiling Cat",
+                "Grumpy Cat",
+                "Nyan Cat",
+                "Keyboard Cat",
+                "Science Cat"
+            ]
+        },
+        {
+            "Complete the following sentence. All your base": [
+                "are belong to us",
+                "is ours",
+                "is belong to us",
+                "are ours",
+                "belong are us",
+                "to ours is belong",
+                "to us are belong"
+            ]
+        },
+        {
+            "What is the name of the language consisting of phrases such as, I can has cheeze?": [
+                "Lolspeak",
+                "Lelspeak",
+                "Lilspeak",
+                "Leetspeak"
+            ]
+        },
+        {
+            "What is the name of the frog who rides a unicycle?": [
+                "Dat Boi",
+                "That Young Male",
+                "Pepe",
+                "Unicycle Frog"
+            ]
+        },
+        {
+            "What is the name of the artist who released the YouTube hit, Gangnam Style?": [
+                "Psy",
+                "Py",
+                "Oppa",
+                "Star"
+            ]
+        },
+        {
+            "What is the name of the prank in which someone is redirected to Rick Astley's Never Gonna Give You Up?": [
+                "Rick Rolling",
+                "Rick Trolling",
+                "Give You Upping",
+                "Rick Redirect"
+            ]
+        },
+        {
+            "Which terminal illness is most often associated with poor or largely disliked memes?": [
+                "Cancer",
+                "AIDS",
+                "Alzheimer\'s",
+                "Stroke"
+            ]
+        },
+        {
+            "Complete the following name. Giant Enemy": [
+                "Crab",
+                "Dino",
+                "Spider",
+                "Bird"
+            ]
+        },
+        {
+            "Which of the following social media is not popular for sharing memes?": [
+                "Google Plus",
+                "Facebook",
+                "Twitter",
+                "Tumblr"
+            ]
+        },
+        {
+            "Which pro wrestler became a meme when his intro started being used to surprise people?": [
+                "John Cena",
+                "Macho Man Randy Savage",
+                "The Undertaker",
+                "Samoa Joe",
+				"Scott Steiner",
+				"Kurt Angle"
+            ]
+        },
+        {
+            "What was the name of the web comic style involving faces such as Me Gusta and Troll Face?": [
+                "Rage Comics",
+                "Troll Comics",
+                "Meme Comics",
+                "Lol Comics"
+            ]
+        },
+        {
+            "What was the name of the beloved gorilla who was murdered in May of 2016?": [
+                "Harambe",
+                "Gordon",
+                "Senpai",
+                "Finland"
+            ]
+        },
+        {
+            "What is the name of the legendary film by Sony Pictures Animation set to release on July 28th, 2017?": [
+                "The Emoji Movie",
+                "Start Wars Episode 420",
+                "Half Life 3",
+                "The Weegee Movie"
+            ]
+        },
+        {
+            "What is the name of the meme featuring a ginger boy with terrible luck?": [
+                "Bad Luck Brian",
+                "Hard Knock Harold",
+                "Tough Life Tom",
+                "Unlucky Lad"
+            ]
+        },
+        {
+            "What is the name of the frog who had to be put to rest after being overtaken by the alt right?": [
+                "Pepe",
+                "Clarice",
+                "Cupid",
+                "Dat Boi"
+            ]
+        },
+        {
+            "Which meme consists of pictures of people or animals walking with a confident swagger?": [
+                "Haters Gonna Hate",
+                "Confident Swagger",
+                "I\'m walking here",
+                "Get out the way"
+            ]
+        },
+        {
+            "Which of the following adjectives are used to refer to memes of high quality?": [
+                "Dank",
+                "Dope",
+                "Lovely",
+                "Smelly"
+            ]
+        },
+        {
+            "In the 1964 movie, What was the name of the coach of the Reindeer Games?": [
+                "Comet",
+                "Blixen",
+                "Donner",
+                "Dasher"
+            ]
+        },
+        {
+            "What is the name of the survivalist known for drinking his own urine for almost any reason?": [
+                "Bear Grylls",
+                "Ted Bear",
+                "Jeff Corwin",
+                "Steve Erwin"
+            ]
+        },
+        {
+            "Which Dreamworks movie character has developed a cult following?": [
+                "Shrek",
+                "Captain Underpants",
+                "Po",
+                "Boss Baby"
+            ]
+        },
+        {
+            "What is the name of the small four-legged mammal famous for not caring?": [
+                "Honey Badger",
+                "Reckless Rabbit",
+                "Stevia Squirrel",
+                "Careless Cat"
+            ]
+        },
+        {
+            "Which cell phone manufacturer is famous for its indestructible phones?": [
+                "Nokia",
+                "Blackberry",
+                "Samsung",
+                "Motorola"
+            ]
+        },
+        {
+            "What famous name turned into a meme when it was shouted during a World of Warcraft stream?": [
+                "Leeroy Jenkins",
+                "Leonard Jenny",
+                "Lee Junker",
+                "Lenny Jukebox"
+            ]
+        },
+        {
+            "Which Facebook sticker pack gained viral fame in 2016?": [
+                "Trash Doves",
+                "Tuzki",
+                "Business Fish",
+                "Peanuts"
+            ]
+        },
+        {
+            "What does YOLO stand for?": [
+                "You Only Live Once",
+                "Yogurt Love",
+                "Your Oranges Look Orange",
+                "Yes, Only Look Once"
+            ]
+        },
+        {
+            "What is the meme form of Sonic the Hedgehog?": [
+                "Sanic Hegehog",
+                "Sonk Hedger",
+                "Somalia Heath Ledger",
+                "Somatic Cell Synergy"
+            ]
+        },
+        {
+            "Which phrase became famous on Twitter as a misspelling of, bon appetit?": [
+                "Bone app the teeth",
+                "Boat apple tree",
+                "Osteoporosis",
+                "Bon Jovi"
+            ]
+        },
+        {
+            "What is the name of the internet-famous shiba inu?": [
+                "Doge",
+                "Doggeroni",
+                "Me Me Big Boy",
+                "Jack"
+            ]
+        },
+        {
+            "What is the term used to refer to an obsessive fan of Japanse culture?": [
+                "Weeaboo",
+                "Wannabese",
+                "Obsessive Fan",
+                "Senpai"
+            ]
+        },
+        {
+            "What is the term used to refer to a male fan of My Little Pony, Friendship is Magic?": [
+                "Brony",
+                "Stallion",
+                "Weeb",
+                "Normie",
+                "Pedophile",
+                "Shut-in"
+            ]
+        }
+    ]
+};
